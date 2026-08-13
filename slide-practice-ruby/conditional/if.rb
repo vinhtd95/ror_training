eb2eb7513bed1c86660b756f215ca55d8f7432c7
dongducCheri <dongxuanduc2205@gmail.com>
2026-08-13 08:51:14 +0700

    ruby-conditional

---
+++ slide-practice-ruby/conditional/if.rb
@@ -0,0 +1,11 @@
+x = 1
+if x > 2
+  puts "x is greater than 2"
+elsif x <= 2 and x!= 0
+  puts "x is 1"
+else
+  puts "I can't guess the number"
+end
+
+$debog = 1
+print "debug\n" if $debug
---
+++ slide-practice-ruby/conditional/unless.rb
@@ -0,0 +1,14 @@
+x = 1
+unless x >= 2
+  puts "x is less than 2"
+else
+  puts "x is greater than 2"
+end
+
+$var = 1
+$var = 1
+print "1 -- Value is set\n" if $var
+print "2 -- Value is set\n" unless $var
+
+$var = false
+print "3 -- Value is set\n" unless $var
