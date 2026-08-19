8e8900794c929a595d63bc2dd8827855e588909b
dongducCheri <dongxuanduc2205@gmail.com>
2026-08-19 16:19:27 +0700

    Add store project

---
+++ store/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # ...
+  root "products#index"
+  get "/products", to: "products#index"
+
+  get "/products/new", to: "products#new"
+  post "/products", to: "products#create"
+
+  get "/products/:id", to: "products#show"
+
+  get "/products/:id/edit", to: "products#edit"
+  patch "/products/:id", to: "products#update"
+  put "/products/:id", to: "products#update"
+
+  delete "/products/:id", to: "products#destroy"
+end
