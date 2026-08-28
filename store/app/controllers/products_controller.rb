2ebe89707d66282f83400e95f00cb2b7e966c344
dongducCheri <dongxuanduc2205@gmail.com>
2026-08-28 11:46:38 +0700

    10.6 extracting partials

--- store/app/controllers/products_controller.rb
+++ store/app/controllers/products_controller.rb
@@ -5,5 +5,33 @@ class ProductsController < ApplicationController
   def show 
     @product = Product.find(params[:id])
   end
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save 
+      redirect_to @product 
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def upadte 
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product 
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
   
+  private
+  def product_params
+    params.expect(product: [:name])
+  end
 end
--- store/config/routes.rb
+++ store/config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   root "products#index"
   get "/products", to: "products#index"
 
-  get "/products/new", to: "products#new"
+  get "/products/new", to: "products#new", as: "new_product"
   post "/products", to: "products#create"
 
-  get "/products/:id", to: "products#show"
+  get "/products/:id", to: "products#show", as: "product"
 
-  get "/products/:id/edit", to: "products#edit"
+  get "/products/:id/edit", to: "products#edit", as: "edit_product"
   patch "/products/:id", to: "products#update"
   put "/products/:id", to: "products#update"
 
