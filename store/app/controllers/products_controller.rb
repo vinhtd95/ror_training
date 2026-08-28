bfa379dce361dcc6bddffb08a164a5541a9c8d51
dongducCheri <dongxuanduc2205@gmail.com>
2026-08-28 13:37:05 +0700

    before-action

--- store/app/controllers/products_controller.rb
+++ store/app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show edit update destroy ]
   def index
     @products = Product.all
   end
+
   def show 
-    @product = Product.find(params[:id])
   end
+
   def new
     @product = Product.new
   end
@@ -17,20 +19,28 @@ class ProductsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
   def edit
-    @product = Product.find(params[:id])
   end
 
-  def upadte 
-    @product = Product.find(params[:id])
+  def update 
     if @product.update(product_params)
       redirect_to @product 
     else
       render :edit, status: :unprocessable_entity
     end
   end
-  
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
   private
+  def set_product 
+    @product = Product.find(params[:id])
+  end
+
   def product_params
     params.expect(product: [:name])
   end
