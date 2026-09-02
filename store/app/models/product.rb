8c41daff2c1cbc8f3082396ae5c608011f22e806
dongducCheri <dongxuanduc2205@gmail.com>
2026-09-02 21:43:37 +0700

    cache & rich text area

--- store/app/models/product.rb
+++ store/app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
+  has_rich_text :description #Dòng này thông báo cho Rails biết sản phẩm có một trường tên là description dạng Rich Text.
   validates :name, presence: true
 end
