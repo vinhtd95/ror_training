027e1d265402d913ad969a9f7f678c4c78f4e9f2
dongducCheri <dongxuanduc2205@gmail.com>
2026-09-02 22:25:36 +0700

    file upload

--- store/app/models/product.rb
+++ store/app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_one_attached :featured_image
   has_rich_text :description #Dòng này thông báo cho Rails biết sản phẩm có một trường tên là description dạng Rich Text.
   validates :name, presence: true
 end
